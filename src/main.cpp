a29fada8fc44cb5eddc5183978ef4791e68a1dba
MadLadMikael <masterspike52@gmail.com>
2026-03-25 01:19:47 -0500

    added other files

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+// redahm - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/redahm_config.h"
+#include "generated/redahm_init.h"
+
+#include "redahm_app.h"
+
+REX_DEFINE_APP(redahm, RedahmApp::Create)
